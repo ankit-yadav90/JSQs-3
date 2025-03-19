8dbcc272c78dd4baa670c2e93da29a114eb9c47f
ankit-yadav90 <ankityadav73699@gmail.com>
2025-03-19 19:35:15 +0530

    prectice

---
+++ first.js
@@ -0,0 +1,9 @@
+//Conditinal Statement
+
+let num = prompt("enter a number:");
+
+if (num % 10 === 0) {
+    console.log(num, " is multipal of 10");
+} else {
+    console.log(num, " is Not a multipal of 10");
+}
